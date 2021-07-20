45db729bc2cd124eeb7e986cae67d33368b72af5
Kylory <72569852+Kylory@users.noreply.github.com>
2021-07-20 20:40:12 +0300

    added files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import shortid from 'shortid';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <p>Hello</p>
+      </>
+    );
+  }
+}
+
+export default App;
